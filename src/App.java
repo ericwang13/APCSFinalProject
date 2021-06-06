8b5e290162470bf7efd7f835cac0239c583fb966
Eric Wang <ericwang13@protonmail.com>
2021-06-06 16:13:40 -0400

    Temporary solution to UnsatisfiedLinkError using absolute path

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
 public class App {
   public static void main(String[] args) throws Exception {
-    System.out.println("Hello, World!");
+    // System.out.println("Hi Stella");
+    System.load("C:/Users/Eric Wang/Desktop/APCSFinalProject/src/dll/opencv_java452.dll");
+    Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+    System.out.println("mat = " + mat.dump());
   }
 }
